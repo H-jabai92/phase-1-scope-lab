efa416b3b7296c4c57293a584ffc50160d741d96
Hasan <h.mjabai92@gmail.com>
2023-10-01 18:49:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+// var customerName = 'bob';
+// function upperCaseCustomerName() {
+//     document.write(customerName.toUpperCase())
+// }
+// upperCaseCustomerName()
+
+var bestCustomer;
+
+// function setBestCustomer() {
+//     var bestCustomer = 'not bob'
+//     document.write(bestCustomer)
+// }
+// setBestCustomer()
+// function overwriteBestCustomer() {
+//      var bestCustomer= 'maybe bob'
+//     document.write(bestCustomer)
+// }
+//  overwriteBestCustomer()
+
+
+// const leastFavoriteCustomer = "yes"
+//  function changeLeastFavoriteCustomer(){
+//     leastFavoriteCustomer = "no"
+     
+// }
+// changeLeastFavoriteCustomer()
